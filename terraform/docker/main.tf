cb13de092e8a43ae225ae82385c7b2d739cd3750
Aarju Sinha <49508889+OCTO01032020-araju@users.noreply.github.com>
2021-02-09 16:51:32 +0530

    Update main.tf

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -1,20 +1,24 @@
+# Setup our aws provider
 provider "aws" {
   access_key  = "AKIARKB35ZMGTWDMM3RQ"
   secret_key  = "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"
   region      = "ap-south-1"
 }
 
-variable "name" {
-    description="Name of the web box at apply"
-}
 
-resource "aws_instance" "doc_try" {
+resource "aws_instance" "example" {
   ami = "ami-073c8c0760395aab8"
   instance_type = "t2.micro"
   key_name = "aaru_terraform"
- 
- 
+  security_groups = ["${aws_security_group.allow_rdp.name}"]
+
   tags = {
-      Name = "${var.name}"
+      Name = "docker"
     }
+} 
+
+resource "aws_security_group" "allow_rdp" {
+  name        = "allow_rdp"
+  description = "Allow rdp traffic"
+
 }
